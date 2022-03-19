184403e977afadbcdf17e16286da33126bb12090
mitzusca <mihai_93_hd@yahoo.com>
2022-03-19 09:05:25 +0000

    Creates random ships on  boards for you and computer

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from random import randint 
+from random import randint
 
 your_board = []
 your_guesses_board = []
@@ -14,18 +14,12 @@ def create_board(board):
     return board
 
 
-c = create_board(your_board)
-
-
 def print_board(board):
     """
-    Print the board and remove quotes, commas and brackets    
+    Print the board and remove quotes, commas and brackets  
     """
     for ind in board:
-        print("".join(ind))
-
-
-print_board(c)
+        print("~".join(ind))
 
 
 def random_num(board):
@@ -33,3 +27,28 @@ def random_num(board):
     Helper function to return a random integer between 0 and size
     """
     return randint(0, len(board)-1)
+
+
+def create_ship(board):
+    """
+    Create 6 random ships on board.
+    """
+    ship_num = 0
+    while ship_num < 6:
+        ship_num = 0
+        ship_x = random_num(board)
+        ship_y = random_num(board)
+        board[ship_x][ship_y] = "X"
+        for x in board:
+            ship_num += x.count("X")
+
+
+def create_boards():
+    """
+    Creating boards and ships for you and computer
+    """
+    create_board(your_board)
+    create_board(computer_board)
+    create_board(your_guesses_board)
+
+print(create_boards())   
