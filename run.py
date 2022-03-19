056de7bdb1967af2b03001d6083edfc755f5c3b3
mitzusca <mihai_93_hd@yahoo.com>
2022-03-19 08:16:19 +0000

    Print the board and remove quotes, commas and brackets

--- run.py
+++ run.py
@@ -4,15 +4,30 @@ your_board = []
 your_guesses_board = []
 computer_board = []
 
+
 def create_board(board):
     """
     Creating a board from 7 lists wich hold question marks
     """
     for a in range(7):
-        board.append(['?'*7])
+        board.append([' ? '*7])
     return board
-b = create_board(your_board)
-print(b)
+
+
+c = create_board(your_board)
+
+
+def print_board(board):
+    """
+    Print the board and remove quotes, commas and brackets    
+    """
+    for ind in board:
+        print("".join(ind))
+
+
+print_board(c)
+
+
 def random_num(board):
     """
     Helper function to return a random integer between 0 and size
