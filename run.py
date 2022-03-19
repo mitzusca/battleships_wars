4ceacfae81a7e36f06a7ccab7c9c92d4e2920e81
mitzusca <mihai_93_hd@yahoo.com>
2022-03-19 07:55:10 +0000

    Creating 7 list as board of for game

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 from random import randint 
 
+your_board = []
+your_guesses_board = []
+computer_board = []
+
+def create_board(board):
+    """
+    Creating a board from 7 lists wich hold question marks
+    """
+    for a in range(7):
+        board.append(['?'*7])
+    return board
+b = create_board(your_board)
+print(b)
 def random_num(board):
     """
     Helper function to return a random integer between 0 and size
