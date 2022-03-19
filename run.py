64c17965806e2252006f46944d3688d9a3912274
mitzusca <mihai_93_hd@yahoo.com>
2022-03-19 11:50:46 +0000

    Finnalizing the game

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ def create_board(board):
     Creating a board from 7 lists wich hold question marks
     """
     for a in range(7):
-        board.append([' ? '*7])
+        board.append([' ? ']*7)
     return board
 
 
@@ -38,9 +38,9 @@ def create_ship(board):
         ship_num = 0
         ship_x = random_num(board)
         ship_y = random_num(board)
-        board[ship_x][ship_y] = "X"
+        board[ship_x][ship_y] = " @ "
         for x in board:
-            ship_num += x.count("X")
+            ship_num += x.count(" @ ")
 
 
 def create_boards():
@@ -67,7 +67,8 @@ def welcome_message():
 
 def your_guesses():
     """
-    
+    Add method for guessing location to change it acordingly
+    and validate if inputs are integers between 1 and 7
     """
     print('This is the computer board!')
     print_board(your_guesses_board)
@@ -85,18 +86,63 @@ def your_guesses():
         chosen_column = int(chosen_column) -1 
         # int -1 because python begins with 0 and it will be used numbers between 1 and 7
         if  (your_guesses_board[chosen_row][chosen_column] == "O" or
-            your_guesses_board[chosen_row][chosen_column] == "X"):
+                your_guesses_board[chosen_row][chosen_column] == "X"):
             print('This time try a new spot!')
         else:
-            repeat False
+            repeat = False
     if computer_board[chosen_row][chosen_column] == "?":
         your_guesses_board[chosen_row][chosen_column] = "X"
         print("\n Bravo, you hit him! ")
     else: 
         your_guesses_board[chosen_row][chosen_column] == "O"
         print('\n Unfortunately you missed!')
+
+
 def computer_guesses():
-    pass
+    """
+    Computer guess at user board using randomly generate coordinates
+    """
+    print('\n Computer turn!')
+    repeat = True
+    chosen_row = random_num(computer_board) 
+    chosen_column = random_num(computer_board)
+    while repeat:
+        if (your_board[chosen_row][chosen_column] == "O" or 
+                your_board[chosen_row][chosen_column] == "X"):
+            chosen_row = random_num(computer_board)
+            chosen_column = random_num(computer_board)
+        else:
+            repeat = False
+    print(f'Computer choosed {chosen_row},{chosen_column}')
+    if your_board[chosen_row][chosen_column] == "?":
+        your_board[chosen_row][chosen_column] = "X"
+        print('Computer hit you!')
+    else:
+        your_board[chosen_row][chosen_column] = "O"
+        print('Super, he missed!')
+
+def new_game():
+    """
+    The main loop to generate a new game
+    """
+    create_boards()   
+    welcome_message()
+     
+    i = 0
+    while i < 30:
+        print(f'This is turn {i+1}/30 \n')
+        your_guesses()
+        print_board(your_board)
+        input("\nPress Enter to continue...")
+        computer_guesses()
+        print('Your board:')
+        input("\nPress Enter to continue...")
+        i += 1
+        if check_winner(your_board) == 6:
+            i = 30
+        elif check_winner(your_guesses_board) == 6:
+            i = 30
+    check_winner_final()
 
 def validate_data(value):
     """
@@ -105,9 +151,36 @@ def validate_data(value):
     """
     try:
         if int(value) > 7 or int(value) < 1 :
-            raise Vallue Error('Please choose an number between 1 and 7!')
-        except ValueError as e:
+            raise ValueError('Please choose an number between 1 and 7!')
+    except ValueError as e:
             print(f'Invalid data {e}, please try again.')
             print('Choose an number between 1 and 7.')
             return False
-        return True
+    return True
+
+
+def check_winner(board):
+    """
+    Sums the number of times " X "  appear in the board.
+    """
+    total = 0
+    for list in board:
+        total += list.count(" X ")
+    return total
+
+
+def check_winner_final():
+    """
+    Check for a winner after 30 turns and report the result to the user
+    """
+    your_result = check_winner(your_guesses_board)
+    computer_result = check_winner(your_board)
+    if your_result > computer_result:
+        print("Congratulations! You WIN!")
+    elif your_result < computer_result:
+        print("Too bad! - the computer had the most hits!")
+    else:
+        print("It was a draw!")
+
+
+new_game()
